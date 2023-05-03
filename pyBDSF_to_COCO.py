25dbd0f59d44fcbb037b352a093dd9b6cbc03599
elastufka <elastufka@gmail.com>
2023-05-03 02:39:39 +0200

    some updates

--- pyBDSF_to_COCO.py
+++ pyBDSF_to_COCO.py
@@ -12,14 +12,26 @@ import argparse
 def get_args_parser():
     parser = argparse.ArgumentParser('pyBDSF_to_COCO', add_help=False)
     parser.add_argument('--catalog', type=str, help="""Full path to catalog file (.txt or .fits)""")
-    parser.add_argument('--image', type=str, help="""Full path to image file (.fits), or else a folder where all images are located.""")
+    parser.add_argument('--image', default=None, type=str, help="""Full path to image file (.fits), or else a folder where all images are located. If no image is passed, it is assumed that the catalog contains a column image_name of corresponding image names.""")
     parser.add_argument('--crop_coords', default=None, type=str, help="""Full path to file where coordinates (SkyCoords) of image crops is located""") 
     #parser.add_argument('--crop_dir', default='.', type=str, help="""Folder where image crops are located""") 
     parser.add_argument('--output_file', type=str, help="""Full path in which to write COCO-format json""")
+    parser.add_argument('--crop_prefix', type=str, help="""Prefix to use for crop names in front of crop_n.npy or crop_n.fits""")
+    parser.add_argument('--start_imid', default=0, type=int, help="""Index at which to start Image IDs. Useful if dataset will contain multiple images.""")
+    parser.add_argument('--start_annid', default=0, type=int, help="""Index at which to start annotation IDs. Useful if dataset will contain multiple images.""")
     parser.add_argument('--test', default=False, type=bool, help="""test""")
 
+    #formatting args
+    parser.add_argument('--bbox_fmt', default="xywh", type=str, help="""Bounding box format: xywh (x0,y0,width,height) or xyxy (x0,y0,x1,y1)""")
+    parser.add_argument('--seg_fmt', default="[xyxy]", type=str, help="""Segmentation format: [[x],[y]] list of x, list of y or [xyxy] list of x0,y0,x1,y1,...xn,yn""")
+    
+    #how to deal with error estimates in catalog? not yet implemented
+    parser.add_argument('--sigma_n', default=1.0, type=float, help="""Account for n sigma error on selected parameters""")
+    parser.add_argument('--sigma_apply', default="ra, dec, maj, min", type=str, help="""Parameters for which to account for n sigma error""")
 
     #args for info and category dictionaries
+    parser.add_argument('--keymap', default=None, type=str, help="""JSON file where map from PyBDSF standard keys are mapped to the keys used in the given dataset""")     
+    parser.add_argument('--annotations_only', default=False, type=bool, help="""only write Images and Annotations dicts to json""")     
     parser.add_argument('--dataset_year', default=None, type=int, help="""Year dataset created""") 
     parser.add_argument('--dataset_version', default="", type=str, help="""Version of dataset""") 
     parser.add_argument('--description', default="", type=str, help="""Description of the dataset""")
@@ -30,24 +42,33 @@ def get_args_parser():
     parser.add_argument('--category_names', default=None, type=str, help="""path to csv file containing category names, or a comma-separated string with names of categories""")
     return parser
 
-def catalog_coords_to_pix(df, wcs):
+def catalog_coords_to_pix(df, wcs, keylist, bbox_fmt = "xywh"):
     """Convert coordinates in catalog from world to pixel. Rather slow for 10K rows right now, can try terality to speed up."""
-    df["bottom_left_coord"] = df.apply(lambda x: ra_dec_to_Skycoord(x["RA"]["DEG"],x["DEC"]["DEG"],x["IM_MAJ"]["DEG"]),axis=1)
-    df["top_right_coord"] = df.apply(lambda x: ra_dec_to_Skycoord(x["RA"]["DEG"],x["DEC"]["DEG"],x["IM_MAJ"]["DEG"],fn=np.add), axis=1)
-    pixarr = [pixvals_from_Skycoord(wcs, row.bottom_left_coord.values[0],row.top_right_coord.values[0]) for i,row in df.iterrows()] #due tomultiindex, row0 is Nan
+    rakey, deckey, majkey, minkey, pakey = keylist
+    
+    if not wcs:
+        #get the wcs from each individual fits file... this will probably be slow
+        assert("image_name" in df.columns)
+
+    df["bottom_left_coord"] = df.apply(lambda x: ra_dec_to_Skycoord(x[rakey]["DEG"],x[deckey]["DEG"],x[majkey]["DEG"]),axis=1)
+    df["top_right_coord"] = df.apply(lambda x: ra_dec_to_Skycoord(x[rakey]["DEG"],x[deckey]["DEG"],x[minkey]["DEG"],fn=np.add), axis=1)
+    pixarr = [pixvals_from_Skycoord(wcs, row) for i,row in df.iterrows()] #due tomultiindex, row0 is Nan
     pixarr = np.array(pixarr)
 
     df["source_xmin"] = pixarr[:,0]
     df["source_ymin"] = pixarr[:,1]
-    #df["source_xmax"] = pixarr[:,2]
-    #df["source_ymax"] = pixarr[:,3]
-    df["source_width"] = pixarr[:,4]
-    df["source_height"] = pixarr[:,5]
-    df["source_bbox"] = df.apply(lambda x: pix_to_bbox(x.source_xmin,x.source_ymin, x.source_width, x.source_height), axis=1)
-    #df["source_bbox"] = df.apply(lambda x: pix_to_bbox(x.source_xmin,x.source_ymin, x.source_xmax, x.source_ymax), axis=1)
-    
-    #segmentation
-    df["segmentation"] = df.apply(lambda x: sky_ellipse_to_path(x["RA"]["DEG"],x["DEC"]["DEG"],x["IM_MAJ"]["DEG"],x["IM_MIN"]["DEG"],x["IM_PA"]["DEG"], wcs), axis=1)
+    if bbox_fmt == "xyxy":
+        df["source_xmax"] = pixarr[:,2]
+        df["source_ymax"] = pixarr[:,3]
+    else: 
+        df["source_xmax"] = pixarr[:,4]
+        df["source_ymax"] = pixarr[:,5]
+    #df["source_width"] = pixarr[:,4]
+    #df["source_height"] = pixarr[:,5]
+    df["source_bbox"] = df.apply(lambda x: pix_to_bbox(x.source_xmin,x.source_ymin, x.source_xmax, x.source_ymax), axis=1)
+     
+    #segmentation. adjust for sigma later
+    df["segmentation"] = df.apply(lambda x: sky_ellipse_to_path(x[rakey]["DEG"],x[deckey]["DEG"],x[majkey]["DEG"],x[minkey]["DEG"],x[pakey]["DEG"], wcs), axis=1)
     return df 
 
 def create_info(year = None, version = None, description = "", contributor = None, url="", date_created=None):
@@ -98,73 +119,93 @@ def create_image_info(image_id, file_name, image_size, license_id=1, coco_url=""
 
     return image_info
 
-def create_annotation_info(annotation_id, image_id, bounding_box=None, segmentation=None):
+def create_annotation_info(annotation_id, image_id, iscrowd, bounding_box=None, segmentation=None):
     if not isinstance(bounding_box, list):
         bbox = bounding_box.values[0]
     else: 
         bbox = bounding_box
-    if not isinstance(segmentation, list):
-        segmentation = [segmentation.values[0][0].tolist(),segmentation.values[0][1].tolist()]
+    if not isinstance(segmentation, list): #it's a series
+        segmentation = segmentation.values.tolist() #[segmentation.values[0][0],segmentation.values[0][1]]
+        #print(type(segmentation), len(segmentation), segmentation[0])
+    #if segmentation_fmt == "[xyxy]":
+    #    segmentation = segmentation_xyxy(segmentation)
+    if not isinstance(iscrowd, list): #it's a series
+        iscrowd = iscrowd.values[annotation_id]
+    else: 
+        iscrowd = iscrowd[annotation_id]
+
     area = int(np.product(bbox[-2:]))
     annotation_info = {
         "id": annotation_id,
         "image_id": image_id,
         "category_id": 1,
-        "iscrowd": 0,
+        "iscrowd": iscrowd,
         "area": area,
         "bbox": bbox,
         "segmentation": segmentation
     } 
     return annotation_info
 
-def create_crop_info(i, image, crop_shape, cdf):
-    image = image[:image.rfind("/")]
-    crop_name = f"{image}/crop_{i}.npy"
+def create_crop_info(i, image, prefix, start_imid, start_annid, crop_shape, cdf):
+    #image = image[:image.rfind("/")]
+    crop_name = f"{prefix}crop_{i}.npy" #{image}/
     crop_json = []
-    image_info = create_image_info(i, crop_name, crop_shape)
+    image_info = create_image_info(i+start_imid, crop_name, crop_shape)
     crop_json.append(image_info)
 
     for j, row in cdf.iterrows():
-        crop_json.append(create_annotation_info(j,i, row.source_bbox, row.segmentation))
+        crop_json.append(create_annotation_info(j+start_annid,i+start_imid, row.iscrowd, row.source_bbox, row.segmentation))
     #with open(output_file, 'w') as f: 
     #    json.dump(crop_json, f)
     return crop_json
 
 def run_main(args): #for now
-    info = create_info(args.dataset_year, args.dataset_version, args.description, args.contributor, args.dataset_url, args.dataset_date)
-    categories = create_categories(args.category_names)
+    if not args.annotations_only:
+        info = create_info(args.dataset_year, args.dataset_version, args.description, args.contributor, args.dataset_url, args.dataset_date)
+        categories = create_categories(args.category_names)
     if args.image.endswith(".fits"):
-        wcs, header, _ = mapdata_from_fits(args.image)
+        wcs, _ = mapdata_from_fits(args.image)
     else: 
-        pass #for now do nothing
+        wcs = None
+
     df = read_catalog(args.catalog)
+
+    keydict = None
+    if args.keymap:
+        keydict = read_keymap(args.keymap)
+    rakey, deckey, majkey, minkey, pakey = keys_used(keydict)
+    keylist = [rakey, deckey, majkey, minkey, pakey]
 
     if args.test:
         df = df.head(50) #for now
-    df = catalog_coords_to_pix(df, wcs)
+        
+    df = catalog_coords_to_pix(df, wcs, keylist, bbox_fmt = args.bbox_fmt)
     #df = pd.read_csv("/home/glados/unix-Documents/AstroSignals/temp_df.csv", header=[0,1])
 
-    if np.isnan(df.RA.DEG[0]):
+    if np.isnan(df[rakey].DEG[0]):
         df.drop(0, inplace = True)
     df.reset_index(inplace=True, drop=True)
 
-    if not args.crop_coords:
-        img_id = 0
+    if args.image and not args.crop_coords:
+        img_id = args.start_imid
         image_info = [create_image_info(img_id, args.image, (wcs.celestial.array_shape))]
         #do in parallel create_annotation_info
         count = len(df)
         vals = df["source_bbox"]
         segmentations = df["segmentation"]
-        async_results = mp_execute_async(create_annotation_info, 4, range(1,count), ifunc = single_async_prep, fnargs = [img_id, vals, segmentations])
+        async_results = mp_execute_async(create_annotation_info, 4, range(1,count), ifunc = single_async_prep, fnargs = [img_id, vals, segmentations, args.seg_fmt])
     
     else: 
         image_info=[]
-        coordlist = np.load(args.crop_coords, allow_pickle=True) #numpy array
+        if not args.image:
+            coordlist = np.load(args.crop_coords, allow_pickle=True) #numpy array
+        else: 
+            coordlist = df.image_name.values
         if not isinstance(coordlist[0][0][0], SkyCoord):
             #deal with this later
             print("not a SkyCoord")
         else:
-            async_results = mp_execute_async(create_crop_info, 4, coordlist, ifunc = crop_async_prep, fnargs = [coordlist, args.image, df, wcs])
+            async_results = mp_execute_async(create_crop_info, 4, coordlist, ifunc = crop_async_prep, fnargs = [coordlist, args.image, args.crop_prefix, args.start_imid, args.start_annid, df, wcs])
 
     annotations = []
     for async_result in async_results:
@@ -176,8 +217,10 @@ def run_main(args): #for now
             annotations.append(res)
 
     annotations = unique_annotation_ids(annotations)
-    mdict = {"info":info,"images":image_info,"annotations":annotations,"categories":categories}
-
+    if not args.annotations_only:
+        mdict = {"info":info,"images":image_info,"annotations":annotations,"categories":categories}
+    else: 
+        mdict = {"images":image_info,"annotations":annotations}
     with open(args.output_file, 'w') as f: 
         json.dump(mdict, f)
 
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 from astropy.io import fits
 from astropy.wcs import WCS
+from astropy.table import Table
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from photutils.aperture import SkyEllipticalAperture
@@ -10,6 +11,7 @@ import matplotlib.patches as patches
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
 import json
+from shapely.geometry import Polygon
 
 def read_catalog(filename):
     """should this always return a multiindex dataframe?"""
@@ -17,20 +19,69 @@ def read_catalog(filename):
         reader = read_txt_catalog
     elif filename.endswith(".fits"):
         reader = read_FITS_catalog
+    elif filename.endswith("csv") or filename.endswith("ascii"):
+        reader = read_csv_catalog
+    elif filename.endswith("cat"):
+        reader = read_cat
     return reader(filename)
 
 def read_txt_catalog(filename):
     return pd.read_fwf(filename, skiprows=8, header=[0,1])
 
 def read_FITS_catalog(filename):
-    pass
+    """specifically for MGCLS catalogs right now"""
+    tab = Table.read(filename, format='fits')
+    cat = tab.to_pandas()
+
+    #unit conversions, renaming columns, multiindex
+    cat['smax_asec'] = [float(row.smax_asec)*u.arcsec.to(u.deg) for _, row in cat.iterrows()] 
+    cat['smin_asec'] = [float(row.smin_asec)*u.arcsec.to(u.deg) for _, row in cat.iterrows()]
+    cat.rename(columns={"RA_deg":"RA","Dec_deg":"DEC","smax_asec":"Maj","smin_asec":"Min","spa_deg":"PA"}, inplace=True)
+    units=["DEG","DEG","Jy","","","DEG","DEG","DEG"]
+    cat.columns = pd.MultiIndex.from_tuples([*zip(cat.columns, units)])
+
+    return cat
+
+def read_cat(filename):
+    cat= pd.read_csv(filename, sep=" ")
+    #cat["src"] = [row.srcid[:row.srcid.find("_")] for _, row in cat.iterrows()]
+    #cat["srcid"] = [int(row.srcid[row.srcid.find("_")+1:]) for _, row in cat.iterrows()]
+    #cat["image_name"] = [f"gaussians_{row.src}/"]
+    cat['majoraxis'] = [float(row.majoraxis[:-6])*u.arcsec.to(u.deg) for _, row in cat.iterrows()]
+    cat['minoraxis'] = [float(row.minoraxis[:-6])*u.arcsec.to(u.deg) for _, row in cat.iterrows()]
+    cat['PA'] = [float(row.PA[:-3]) for _, row in cat.iterrows()]
+    cat.rename(columns={"ra":"RA","dec":"DEC","majoraxis":"Maj","minoraxis":"Min"}, inplace=True)
+    units=["DEG","DEG","Jy","","","DEG","DEG","DEG"]
+    cat.columns = pd.MultiIndex.from_tuples([*zip(cat.columns, units)])
+    return cat
+
+def read_csv_catalog(filename, skiprows=8):
+    # for ascii, sep= " "
+    return pd.read_csv(filename, skiprows=skiprows, header=[0,1])
+
+def read_keymap(filename):
+    """default PyBDSF keys: https://pybdsf.readthedocs.io/en/latest/write_catalog.html"""
+    with open(filename) as f: 
+        keydict = json.load(f)
+    return keydict
+
+def keys_used(keydict):
+    if not keydict:
+        return "RA","DEC","Maj","Min","PA"
+    else:
+        rakey = keydict["RA"]
+        deckey = keydict["DEC"]
+        majkey = keydict["Maj"]
+        minkey = keydict["Min"]
+        pakey = keydict["PA"]
+        return rakey, deckey, majkey, minkey, pakey
 
 def mapdata_from_fits(filename):
     with fits.open(filename) as f: 
         header = f[0].header
         wcs = WCS(header)
-        dat = f[0].data.squeeze()
-    return wcs, header, dat
+        dat = f[0].data #.squeeze()
+    return wcs, dat
 
 def img_bl_tr(img, wcs = None):
     """Determine bottom left and top right of image in astrophysical coordinates"""
@@ -61,19 +112,38 @@ def sorted_coords(x1,x2,y1,y2):
     y1, y2 = min([y1,y2]), max([y1,y2])
     return x1,x2,y1,y2
 
-def pixvals_from_Skycoord(wcs, bottom_left_coord, top_right_coord):
-    if np.isnan(bottom_left_coord.ra.value):
+def pixvals_from_Skycoord(wcs, row):
+    blc = row.bottom_left_coord.values[0]
+    trc = row.top_right_coord.values[0]
+    if np.isnan(blc.ra.value):
         return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
-    x1, y1 = wcs.celestial.world_to_pixel(bottom_left_coord)
+    if wcs is None:
+        wcs, _ = mapdata_from_fits(row.image_name)
+        #get wcs from file name
+    x1, y1 = wcs.celestial.world_to_pixel(blc)
     x1 = int(np.round(x1,0))
     y1 = int(np.round(y1,0))
-    x2, y2 = wcs.celestial.world_to_pixel(top_right_coord)
+    x2, y2 = wcs.celestial.world_to_pixel(trc)
     x2 = int(np.round(x2,0))
     y2 = int(np.round(y2,0))
     x1, x2, y1,y2 = sorted_coords(x1,x2,y1,y2)
     width = x2-x1
     height = y2-y1
     return x1,y1,x2,y2,width, height
+
+# def pixvals_from_Skycoord(wcs, bottom_left_coord, top_right_coord):
+#     if np.isnan(bottom_left_coord.ra.value):
+#         return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
+#     x1, y1 = wcs.celestial.world_to_pixel(bottom_left_coord)
+#     x1 = int(np.round(x1,0))
+#     y1 = int(np.round(y1,0))
+#     x2, y2 = wcs.celestial.world_to_pixel(top_right_coord)
+#     x2 = int(np.round(x2,0))
+#     y2 = int(np.round(y2,0))
+#     x1, x2, y1,y2 = sorted_coords(x1,x2,y1,y2)
+#     width = x2-x1
+#     height = y2-y1
+#     return x1,y1,x2,y2,width, height
 
 def sky_ellipse_to_path(ra, dec, maj, minor, pa, wcs, units=[u.deg, u.deg, u.deg, u.deg], pix_offset=None):
     """Catalog values of a 2D ellipse to pixel mask"""
@@ -97,10 +167,11 @@ def path_from_patch(patch):
     #I interpret poly as list of polygons, defined by the vertexes, like [[x1,y1,x2,y2…xN,yN],…[x1,y1,x2,y2…xN,yN] ], where the coordinates are of the same scale as the image.
     #The masks, encoded this way, are shown correctly by CocoAPI [1]
     #from https://github.com/facebookresearch/Detectron/issues/100
-    return vertices.T.tolist() #hope this doesn't have to be int
+    return vertices.T.tolist() #hope this doesn't have to be int #replace .T
 
-def get_crop_shape(example_crop_coords, wcs):
-    _,_,_,_,w,h = pixvals_from_Skycoord(wcs, example_crop_coords[0][0], example_crop_coords[1][0])
+def get_crop_shape(example_crop_coords, wcs, image_name = None):
+    row = pd.DataFrame({"bottom_left_coord":example_crop_coords[0][0], "top_right_coord":example_crop_coords[0][0], "image_name": image_name})
+    _,_,_,_,w,h = pixvals_from_Skycoord(wcs, row[0])
     return w,h
 
 def adjust_bbox_pixvals(bbox, xmin, ymin):
@@ -109,38 +180,72 @@ def adjust_bbox_pixvals(bbox, xmin, ymin):
     return [bbox[0]-xmin, bbox[1]-ymin, bbox[2],bbox[3]]
 
 def adjust_segmentation_pixvals(seg, x1,y1):
-    return [np.array(seg[0])-x1, np.array(seg[1])-y1] 
+    return [np.array(seg[0])-x1, np.array(seg[1])-y1]
+    #return [[x-x1, y-y1] for x,y in seg] 
 
-def single_crop_catalog(df, cc, wcs):
+def segmentation_xyxy(segmentation):
+    """re-format segmentation from [list of x, list of y] to single list of [x1,y1,x2,y2,...xn,yn]"""
+    m2 = []
+    for x,y in zip(*segmentation):
+        m2.append(x)
+        m2.append(y)
+    return [m2]
+
+def check_overlap(segs):
+    """check if any segmentations in a single crop/image overlap"""
+    iscrowd = np.zeros(len(segs)).astype(int)
+    #use shapely Polygon
+    polygons = [Polygon(np.array(s).T) for s in segs] #replace .T
+
+    for i,p in enumerate(polygons):
+        inter = [j for j,p2 in enumerate(polygons) if i !=j and p.intersects(p2)]
+        if inter != []:
+            for k in inter:
+                #smallcat.append({"image_id":n,"bbox":jdf.bbox[k],"segmentation":jdf.segmentation[k]})
+                iscrowd[k] = 1
+    return iscrowd.tolist()
+
+def single_crop_catalog(df, cc, wcs, rakey="RA", deckey="DEC"):
     """Return catalog information for a single crop"""
-    bl = cc[0][0]
-    blra = bl.ra.value
-    tr = cc[1][0]
-    trra = tr.ra.value
-    bldec =bl.dec.value
-    trdec = tr.dec.value
-    dd = df.where(df.RA.DEG > min((blra,trra))).where(df.RA.DEG < max((blra,trra))).dropna(how='all')
-    res = dd.where(dd.DEC.DEG > min((bldec,trdec))).where(dd.DEC.DEG < max((bldec,trdec))).dropna(how='all')
-    #shift all pixel coordinates relative to crop position in primary image
-    x1,y1,_,_,_,_ = pixvals_from_Skycoord(wcs, bl, tr)
-    res["source_xmin"] -= x1
-    res["source_ymin"] -= y1
-    #res["source_bbox"] = [adjust_bbox_pixvals(row.source_bbox, x1,y1) for _,row in res.iterrows()]
-    #res.apply(lambda x: adjust_bbox_pixvals(x["source_bbox"], x1,y1), axis=1)
-    res["segmentation"] = [adjust_segmentation_pixvals(row.segmentation.values[0],x1,y1) for _,row in res.iterrows()] #res.apply(lambda x: [x["segmentation"][0] - x1, x["segmentation"][1] - y1], axis=1)
+    if wcs is None:
+        res = df.where(df.image_name == cc).dropna(how='all')#
+    else:
+        bl = cc[0][0]
+        blra = bl.ra.value
+        tr = cc[1][0]
+        trra = tr.ra.value
+        bldec =bl.dec.value
+        trdec = tr.dec.value
+        dd = df.where(df[rakey].DEG > min((blra,trra))).where(df[rakey].DEG < max((blra,trra))).dropna(how='all')
+        res = dd.where(dd[deckey].DEG > min((bldec,trdec))).where(dd[deckey].DEG < max((bldec,trdec))).dropna(how='all')
+        #shift all pixel coordinates relative to crop position in primary image
+        x1,y1,_,_,_,_ = pixvals_from_Skycoord(wcs, bl, tr)
+        res["source_xmin"] -= x1
+        res["source_ymin"] -= y1
+        res["source_bbox"] = [adjust_bbox_pixvals(row.source_bbox, x1,y1) for _,row in res.iterrows()]
+        #res.apply(lambda x: adjust_bbox_pixvals(x["source_bbox"], x1,y1), axis=1)
+        res["segmentation"] = [adjust_segmentation_pixvals(row.segmentation.values[0],x1,y1) for _,row in res.iterrows()] #res.apply(lambda x: [x["segmentation"][0] - x1, x["segmentation"][1] - y1], axis=1)
+    
+    res["iscrowd"] = check_overlap(res["segmentation"])
     return res
 
-def single_async_prep(i,img_id, vals, segmentations):
+def single_async_prep(i,img_id, vals, segmentations, segmentation_fmt):
     val = vals[i]
     seg = segmentations[i]
-    return i, img_id, val, seg
+    iscrowd = check_overlap(segmentations)
+    if segmentation_fmt == "[xyxy]":
+        seg = segmentation_xyxy(seg)
+    return i, img_id, iscrowd, val, seg
 
-def crop_async_prep(i, coordlist, crop_dir, df, wcs):
+def crop_async_prep(i, coordlist, crop_dir, prefix, start_imid, start_annid, df, wcs):
     cc = coordlist[i]
-    crop_shape = get_crop_shape(cc, wcs)
+    image_name = None
+    if wcs is None:
+        image_name = df["image_name"][i]
+    crop_shape = get_crop_shape(cc, wcs, image_name)
     cdf = single_crop_catalog(df, cc, wcs)
     cdf.reset_index(inplace=True,drop=True)
-    return i, crop_dir, crop_shape, cdf
+    return i, crop_dir, prefix, start_imid, start_annid, crop_shape, cdf
 
 def mp_execute_async(func, nprocesses, iterover, ifunc=None, fnargs=None):
     async_results = []
@@ -165,18 +270,89 @@ def unique_annotation_ids(annotations):
             annotations[i]['id'] = i
     return annotations
 
+def train_val_split(full_json, train_dir, val_dir, json_only=False, **kwargs):
+    """do sklearn train_test_split, move images into the correct folders, and split the json"""
+    with open(full_json) as f: 
+        sdict = json.load(f)
+    from sklearn.model_selection import train_test_split
+    import shutil
+
+    idf = pd.DataFrame(sdict["images"])
+    adf = pd.DataFrame(sdict["annotations"])
+    X_train,X_val = train_test_split(idf.id.values, **kwargs)
+
+    trainims, trainanns = [],[]
+    valims, valanns = [],[]
+    akeys = ['id','image_id','iscrowd','category_id']
+    ikeys = ['id','width','height']
+    #move images
+    for x in X_train:
+        imfile = idf.where(idf.id == x).dropna(how='all')
+        for k in ikeys:
+            imfile[k] = imfile[k].astype(int)
+        if not json_only:
+            shutil.move(imfile.file_name.values[0], train_dir)
+        #adjust imfile name
+        imfile['file_name'] = [f[f.rfind("/")+1:] for f in imfile['file_name']]
+        trainims.extend(imfile.to_dict(orient='records'))
+        anns = adf.where(adf.image_id == x).dropna(how='all')
+        for k in akeys:
+            anns[k] = anns[k].astype(int)
+        trainanns.extend(anns.to_dict(orient='records'))
+        
+
+    for x in X_val:
+        imfile = idf.where(idf.id == x).dropna(how='all')
+        for k in ikeys:
+            imfile[k] = imfile[k].astype(int)
+        if not json_only:
+            shutil.move(imfile.file_name.values[0], val_dir)
+        imfile['file_name'] = [f[f.rfind("/")+1:] for f in imfile['file_name']]
+        valims.extend(imfile.to_dict(orient='records'))
+        anns = adf.where(adf.image_id == x).dropna(how='all')
+        for k in akeys:
+            anns[k] = anns[k].astype(int)
+        valanns.extend(anns.to_dict(orient='records'))
+
+
+    #split JSON
+    traininfo = sdict['info'].copy()
+    valinfo = sdict['info'].copy()
+    traininfo['description'] += " train"
+    valinfo['description'] += " val"
+    train_json =  {"info":traininfo,"images":trainims,"annotations":trainanns,"categories":sdict['categories']}
+    val_json =  {"info":valinfo,"images":valims,"annotations":valanns,"categories":sdict['categories']}
+    
+    #return train_json, val_json
+    
+    with open(f"{full_json[:full_json.rfind('.')]}_train.json",'w') as f:
+         json.dump(train_json,f)
+         
+    with open(f"{full_json[:full_json.rfind('.')]}_val.json",'w') as f:
+         json.dump(val_json,f)
+
 def plot_image_catalog(image, annotations_json, bounding_boxes = True, segmentations = False, **kwargs):
     """Plot the image and the associated bounding boxes and/or segmentations on top for verification"""
     try:
-        wcs ,_, dat = mapdata_from_fits(image)
+        wcs, dat = mapdata_from_fits(image)
+        dat = dat.squeeze()
     except OSError:
         dat = np.load(image)
-    with open(annotations_json) as f:
-        data = json.load(f)
-    jdf = pd.DataFrame(data[1:]) #[0] is image annotations
+        crop_number = int(image[image.rfind("_")+1:image.rfind(".")])
+    if isinstance(annotations_json, list): #it's already the dict item
+        jdf = pd.DataFrame(annotations_json)
+        try:
+            jdf = jdf.where(jdf.image_id == crop_number).dropna(how='all')
+        except UnboundLocalError:
+            pass
+    else:
+        with open(annotations_json) as f:
+            data = json.load(f)
+        jdf = pd.DataFrame(data["annotations"]) #[0] is image annotations
 
     if bounding_boxes:
         bboxes = jdf.bbox
+        #print(bboxes)
     if segmentations:
         segs = jdf.segmentation
 
@@ -190,6 +366,12 @@ def plot_image_catalog(image, annotations_json, bounding_boxes = True, segmentat
     if segmentations:
         for seg in segs:
             #draw ellipse
-            ax.plot(seg[0],seg[1],'r')    
+            if len(seg) == 1:
+                segx = [x for i,x in enumerate(seg[0])if i%2 == 0]
+                segy = [x for i,x in enumerate(seg[0])if i%2 == 1]
+            else: 
+                segx = seg[0]
+                segy = seg[1]
+            ax.plot(segx,segy,'r')    
 
     return fig
